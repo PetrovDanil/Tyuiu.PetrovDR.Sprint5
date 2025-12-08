5f9789f90c4bdeab229d7e66b7013f8feb5905b0
tokyopium ㅤ <olilhjgjg2@gmail.com>
2025-12-09 01:08:08 +0500

    task2

---
+++ Tyuiu.PetrovDR.Sprint5.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PetrovDR.Sprint5.Task2.V12.Lib;
+
+namespace Tyuiu.PetrovDR.Sprint5.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}
---
+++ Tyuiu.PetrovDR.Sprint5.Task2.V12/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.PetrovDR.Sprint5.Task2.V12.Lib;
+
+
+
+namespace Tyuiu.PetrovDR.Sprint5.Task2.V12
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            void PrintCenteredLine(string text, int totalWidth)
+            {
+                var padding = totalWidth - text.Length - 2;
+                Console.WriteLine($"* {text}{new string(' ', padding)}*");
+            }
+            var width = 100;
+
+
+
+            DataService ds = new DataService();
+
+            PrintCenteredLine("Спринт #5 | Выполнил: Петров Д. Р. | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("Спринт #5", width);
+            PrintCenteredLine("Тема: Обработка файлов", width);
+            PrintCenteredLine("Задание #2", width);
+            PrintCenteredLine("Вариант #12", width);
+            PrintCenteredLine("Выполнил: Петров Данил Романович | РППб-25-1", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("УСЛОВИЕ:", width);
+            Console.WriteLine("Дан двумерный целочисленный массив 3 на 3 элементов, заполненный значениями с клавиатуры.", width);
+            Console.WriteLine("Заменить положительные элементы массива на 1, отрицательные на 0. Результат сохранить в файл.", width);
+            Console.WriteLine(new string('*', width));
+
+            PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+
+            int[,] matrix = { { -5, -5, 9 }, { -7, 3, -4 }, { 8, 7, 9 } };
+
+            Console.WriteLine(new string('*', width));
+            PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+
+            string res = ds.SaveToFileTextData(matrix);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создaн!");
+
+            Console.ReadKey();
+        }
+    }
+}
